c366f06a457a4a4cc2bf86f2a631893b941b1070
Aditya Pradita <pradita397@gmail.com>
2020-04-04 11:09:16 +0700

    Separate Exhibitor Card component and make a functional search

---
+++ src/components/ExhibitorCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="list-unstyled exhibitors-list">
+        <li v-for="exhibitor in exhibitors" :key="exhibitor.id_exhibitor">
+            <div class="exhibitor-logo-container">
+            <img :src="exhibitor.logo" />
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import axios from 'axios';
+
+  export default {
+      name: 'ExhibitorCard',
+      props: ['exhibitors'],
+  }
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,17 @@
   <div class="home">
     <header>
       <div class="container">
-        
+        <div class="row search-row">
+          <div class="col-12">
+            <input class="search-box" type="text" v-model="search" placeholder="Search Exhibitor..." />
+
+          </div>
+        </div>
       </div>
     </header>
     <div class="content">
       <div class="container">
-          <ul class="list-unstyled exhibitors-list">
-              <li v-for="exhibitor in exhibitors" :key="exhibitor.id_exhibitor">
-                  <div class="exhibitor-logo-container">
-                    <img :src="exhibitor.logo" />
-                  </div>
-              </li>
-          </ul>
+          <ExhibitorCard :exhibitors="filteredExhibitorsBySearch" />
       </div>
     </div>
   </div>
@@ -22,15 +21,21 @@
 <script>
   // @ is an alias to /src
   import axios from 'axios';
+  import ExhibitorCard from '../components/ExhibitorCard.vue';
   const event_id = "1b59351267938da712d19d57889c7f565cab96406e27a874607b90492a2845232f233a2b72bb4ae006a79b53f95aef054935c50b64d6a2a03cfe5cc75cbcd5cd";
   const client_id = "45426";
   const apiUrl = `https://api.jublia.com/buzz/v2/directory/search?event_id=${event_id}&client_id=${client_id}`;
 
   export default {
     name: 'Home',
+    components: {
+        ExhibitorCard
+    },
     data() {
       return {
-        exhibitors : []
+        exhibitors : [],
+        filteredExhibitors : [],
+        search: '',
       }
     },
 
@@ -41,17 +46,19 @@
         .then(response => {
           // JSON responses are automatically parsed.
           this.exhibitors = response.data.searchResult;
-
-        })
-        .catch(e => {
-          this.countries.push(e)
+          this.filteredExhibitors = this.exhibitors.slice(0,12);
         })
     },
     mounted() {
       
     },
     computed: {
-      
+      filteredExhibitorsBySearch() {
+        return this.exhibitors
+          .filter(exhibitor =>
+            exhibitor.company_name.toLowerCase().includes(this.search.toLowerCase())
+          )
+      },
     }
   }
 </script>
