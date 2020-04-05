7fba0177b7ab50465326f738731cd149dcf6876d
Aditya Pradita <pradita397@gmail.com>
2020-04-05 18:49:19 +0700

    Add comments in Exhibitor Cards as mini-documentation

--- src/components/ExhibitorCard.vue
+++ src/components/ExhibitorCard.vue
@@ -1,9 +1,12 @@
+<!--This component is to render the list of exhibitors in the form of cards -->
 <template>
     <ul class="list-unstyled exhibitors-list">
         <li v-for="exhibitor in exhibitors" :key="exhibitor.id_exhibitor">
             <header class="exhibitor-card-header">
                 <div class="exhibitor-logo-container">
                     <img :src="exhibitor.logo" :alt="exhibitor.company_name" />
+
+                    <!-- Conditional Render Bookmark based on if bookmark have value in exhibitor or not -->
                     <a v-if="exhibitor.bookmark !== 0" class="bookmark active" title="bookmarked">
                         <span class="icon bookmark-icon"></span>
                     </a>
@@ -16,9 +19,12 @@
                 <h3>{{exhibitor.company_name}}</h3>
             </div>
             <div class="exhibitor-body">
+                <!-- Conditional Render Booth based on if booth exist in an exhibitor or not -->
                 <a v-if="exhibitor.booth" class="exhibitor-booth" href="#">{{exhibitor.booth}}</a>
+
                 <a v-if="!exhibitor.booth" class="exhibitor-booth no-booth" href="#">No Booth</a>
 
+                <!-- Conditional Render Message with notif or without notide based on value of messaged in exhibitor -->
                 <a v-if="exhibitor.messaged !== 0" class="message active" title="message">
                     <span class="icon message-icon"></span>
                     Messaged
